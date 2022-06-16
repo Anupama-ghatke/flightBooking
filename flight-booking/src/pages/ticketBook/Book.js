7117ffb4216c893afd37d89d240580aee93c5a88
Anupama <85158475+Anupama-ghatke@users.noreply.github.com>
2022-06-17 00:33:03 +0530

    Update Book.js

--- flight-booking/src/pages/ticketBook/Book.js
+++ flight-booking/src/pages/ticketBook/Book.js
@@ -4,98 +4,98 @@ import { onValue, ref, set } from "firebase/database";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { firebaseAuth, firebaseDatabase } from "../../backend/firebaseHandler";
+
 import './Book.css';
 
-const TicketBook = () => {
-  const nav = useNavigate();
+const UserDashBoard = () => {
+  const navigate = useNavigate();
 
-  const [Flight, setFlight] = useState([]);
+  const [flightList,setFlightList] = useState([]);
   const [ids,setIds] = useState([]);
-  const [date, setDate] = useState('');
-  const [userid, setUserid] = useState();
+  const [date, setDate] = useState(null);
+  const [uid, setUid] = useState();
 
   useEffect(() => {
     onAuthStateChanged(firebaseAuth, (user) => {
       if (user) {
-        setUserid(user.userid);
+        setUid(user.uid);
       }else{
-        nav("/login")
+        navigate("/login")
       }
     });
-       const fref = ref(firebaseDatabase,'Flights-Record')
-        onValue(fref,(snapshot) => {
-            setFlight(Object.values(snapshot.val()));
-            setIds(Object.keys(snapshot.val()));
-        },{onlyOnce:true})
-     },[])
+    const fref = ref(firebaseDatabase,'Flights-Record')
+    onValue(fref,(snapshot) => {
+        setFlightList(Object.values(snapshot.val()));
+        setIds(Object.keys(snapshot.val()));
+    },{onlyOnce:true})
+  },[])
 
   const handleClick = (e) => {
-      const fbref = ref(firebaseDatabase,`Client-Record/${userid}`)
+      const fbref = ref(firebaseDatabase,`Client-Record/${uid}`)
       const id = e.target.id;
       onValue(fbref, async (snapshot) => {
-      const userref = ref(firebaseDatabase,`Book-List/${id}/${userid}`)
+      const userref = ref(firebaseDatabase,`Bookings/${id}/${uid}`)
       await set(userref,snapshot.val())
       alert("Ticket Booked!")
       })
   }
 
+  const handleLogout = () => {
+    signOut(firebaseAuth);
+  }
 
     return (
         <div style={{backgroundColor:"lavender" }}>
             <hr/>
-            <h1>Welcome!</h1>
-            <Button variant="contained" onClick={() => { signOut(firebaseAuth); }} sx={{width:'180px', height:'50px', position:'absolute', top:'125px', right:'20px'}}>Log Out</Button>  
+            <h1>Welcome</h1>
+            <Button variant="contained" onClick={handleLogout} sx={{width:'180px', height:'50px', position:'absolute', top:'125px', right:'20px'}}>Log Out</Button>
             <div style={{height:"40px",  width:"330px",marginBottom:"50px"}}>
                 <InputLabel>Select Date</InputLabel>
                 <TextField sx={{width:"330px"}} id="outlined-basic" variant="outlined" type={'date'} value={date} onChange={(e) => setDate(e.target.value)}/>
             </div>
-            <hr/>
-
-            <div className="list-Container">
-                {Flight.map((Flight,index) => {
-                    if(date == Flight.date){
-                        return(
-
-                            <div key={ids[index]} className="Data-Container">
-                                <div>
-                                    <p>Airline</p>
-                                    <h3>{Flight.airline}</h3>
-                                </div>
-                                <div>
-                                    <p>Date</p>
-                                    <h3>{Flight.date}</h3>
-                                </div>
-                                <div>
-                                    <p>Departure Time</p>
-                                    <h3>{Flight.departureTime}</h3>
-                                </div>
-                                <div>
-                                    <p>Arrival Time</p>
-                                    <h3>{Flight.arrivalTime}</h3>
-                                </div>
-                                <div>
-                                    <p>Boarding Point</p>
-                                    <h3>{Flight.boarding}</h3>
-                                </div>
-                                <div>
-                                    <p>Destination</p>
-                                    <h3>{Flight.destination}</h3>
-                                </div>
-                                <div>
-                                    <p>Cost</p>
-                                    <h3>{Flight.cost}</h3>
-                                </div>
-                                <div>
-                                    <Button variant="contained" onClick={handleClick} id={ids[index]} sx={{height:"40px", width:"150px", marginTop:"35px"}}>Book</Button>
-                                </div>
-                            </div>
-                            )}
-                        })
-                    }
-            
+          <div className="list-Container">
+            {flightList.map((flight,index) => {
+               if(date == flight.date){
+                return(
+                  <div key={ids[index]} className="Data-Container">
+                        <div>
+                        <p>Airline</p>
+                        <h2>{flight.airline}</h2>
+                        </div>
+                        <div>
+                            <p>Date</p>
+                            <h2>{flight.date}</h2>
+                        </div>
+                        <div>
+                            <p>Departure Time</p>
+                             <h2>{flight.departureTime}</h2>
+                        </div>
+                        <div>
+                            <p>Arrival Time</p>
+                            <h2>{flight.arrivalTime}</h2>
+                        </div>
+                        <div>
+                            <p>Boarding Point</p>
+                            <h2>{flight.boarding}</h2>
+                        </div>
+                        <div>
+                            <p>Destination</p>
+                            <h2>{flight.destination}</h2>
+                        </div>
+                        <div>
+                            <p>Cost</p>
+                            <h2>{flight.cost}</h2>
+                        </div>
+                        <div>
+                            <Button variant="contained" onClick={handleClick} id={ids[index]}>Book</Button>
+                        </div>
+                       </div>
+                )
+               }
+            })}
             </div>
         </div>
     )
 }
 
-export default TicketBook;
+export default UserDashBoard;
